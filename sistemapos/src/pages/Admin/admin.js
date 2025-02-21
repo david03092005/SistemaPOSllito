ffa153fc999af77b48a26fcf2d375537b17c2d21
tabaressara <tabaressara1110@javerianacali.edu.co>
2025-02-21 14:57:44 -0500

    prueba graf crear empleado

--- sistemapos/src/pages/Admin/admin.js
+++ sistemapos/src/pages/Admin/admin.js
@@ -8,7 +8,30 @@ function Admin() {
         <div class="container text-center">
             <div class="row row-cols-3 g-5 grid text-center">
                 <div className="col">
-                    <button type="button" class="btn btn-outline-primary btn-lg w-80">Primary</button>
+                    <div class="mb-3">
+                        <label for="cedula" class="form-label">Número de Cédula</label>
+                        <input type="number" id="cedula" class="form-control"  required/>
+                    </div>
+                    <div class="mb-3">
+                        <label for="nombre" class="form-label">Nombre</label>
+                        <input type="text" id="nombre" class="form-control"  required/>
+                    </div>
+                    <div class="mb-3">
+                        <label for="contrasena" class="form-label">Contraseña</label>
+                        <input type="text" id="contrasena" class="form-control"  required/>
+                    </div>
+                    <div class="mb-3">
+                        <label for="contrasena" class="form-label">Contraseña</label>
+                        <input type="text" id="contrasena" class="form-control"  required/>
+                    </div>
+                    <div class="btn-group" role="group" name="comotuquieras">
+                        <input type="radio" class="btn-check" name="role" id="admin" value="admin" required/>
+                        <label class="btn btn-outline-primary" for="admin">Administrador</label>
+
+                        <input type="radio" class="btn-check" name="role" id="vendedor" value="vendedor" required/>
+                        <label class="btn btn-outline-secondary" for="vendedor">Vendedor</label>
+                    </div>
+
                 </div>
                 <div class="col">
                     <button type="button" class="btn btn-outline-secondary btn-lg w-80">Secondary</button>
